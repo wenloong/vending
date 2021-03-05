928fbdc27ab714f96b9ffddd45e50d0273a0cd7c
wenloong <wenloongng@gmail.com>
2021-03-05 01:09:39 -0800

    added dynamic images for locations

--- client/src/components/VendingMachines/VendingMachineItem.tsx
+++ client/src/components/VendingMachines/VendingMachineItem.tsx
@@ -1,22 +1,27 @@
+import { useState } from 'react'
 import { VendingMachine } from './VendingMachine'
 import { useHistory } from 'react-router-dom'
-
-//import newyork from '../../assets/images/newyork.jpg'
-import sanfrancisco from '../../assets/images/sanfrancisco.jpg'
 
 interface Props {
   vendingmachine: VendingMachine
 }
 
 const VendingMachineItem = (props: Props) => {
-  const { vendingmachine } = props;
+  const { vendingmachine } = props
   const history = useHistory()
+  const [image, setImage] = useState();
+
+  const loadImage = (imageName: any) => {
+    import(`../../assets/images/${imageName}.jpg`).then(image => {
+      setImage(image.default)
+    })
+  }
 
   return (
     <div className="VendingMachineItem" onClick={() => history.push(`/vending-machine/${vendingmachine._id}`)}>
-      <img className="VendingMachineItem__image" src={sanfrancisco} alt="Location"/>
+      { loadImage(vendingmachine.location.replace(/\s/g, '').toLowerCase()) }
+      { image && <img className="VendingMachineItem__image" src={image} alt={vendingmachine.location}/> }
       <p className="VendingMachineItem__text">{vendingmachine.location}</p>
-
     </div>
   )
 }
